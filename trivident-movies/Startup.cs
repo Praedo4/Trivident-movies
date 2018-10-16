14344b106110aaabea479effc3c963e04cf597d1
Praedo <praedo4421@gmail.com>
2018-10-16 14:20:25 +0200

    Add project files.

---
+++ trivident-movies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trivident_movies.Startup))]
+namespace trivident_movies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
